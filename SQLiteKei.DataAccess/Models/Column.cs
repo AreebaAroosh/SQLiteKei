9cb38b3f2d1fea1fe70f84358074f3e32fa47cad
ShaezoNai <matthias.rueckbrodt@gmail.com>
2016-06-04 17:53:22 +0200

    Add model for columns to DataAccess

---
+++ SQLiteKei.DataAccess/Models/Column.cs
@@ -0,0 +1,17 @@
+﻿namespace SQLiteKei.DataAccess.Models
+{
+    public class Column
+    {
+        public int Id { get; set; }
+
+        public string Name { get; set; }
+
+        public string Type { get; set; }
+
+        public bool IsNotNullable { get; set; }
+
+        public object DefaultValue { get; set; }
+
+        public bool IsPrimary { get; set; }
+    }
+}
