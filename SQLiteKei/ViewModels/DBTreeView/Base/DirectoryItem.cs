71141186c23baec41851053a567bd93b712536f4
ShaezoNai <matthias.rueckbrodt@gmail.com>
2016-06-05 16:28:18 +0200

    Change DirectoryItem Items from List<T> to ObservableCollection<T> so that a TreeView can observe changes to the children

--- SQLiteKei/ViewModels/DBTreeView/Base/DirectoryItem.cs
+++ SQLiteKei/ViewModels/DBTreeView/Base/DirectoryItem.cs
@@ -1,14 +1,14 @@
-﻿using System.Collections.Generic;
+﻿using System.Collections.ObjectModel;
 
 namespace SQLiteKei.ViewModels.DBTreeView.Base
 {
     public abstract class DirectoryItem : TreeItem
     {
-        public List<TreeItem> Items { get; set; }
+        public ObservableCollection<TreeItem> Items { get; set; }
 
         public DirectoryItem()
         {
-            Items = new List<TreeItem>();
+            Items = new ObservableCollection<TreeItem>();
         }
     }
 }
