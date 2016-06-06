8674143b20db4c938f7254b09b817842a3630466
ShaezoNai <m.rueckbrodt@sycat.com>
2016-06-06 20:58:57 +0200

    Add DbTestBase to SQLiteKei.IntegrationTests

---
+++ SQLiteKei.IntegrationTests/Base/DbTestBase.cs
@@ -0,0 +1,36 @@
+﻿#region usings
+
+using System.Data.SQLite;
+using System.IO;
+
+using NUnit.Framework;
+
+#endregion
+
+namespace SQLiteKei.IntegrationTests.Base
+{
+    public class DbTestBase
+    {
+        private const string testDirectory = @"C:\Test";
+
+        [OneTimeSetUp]
+        public void CreateDatabase()
+        {
+            if (!Directory.Exists(testDirectory))
+            {
+                Directory.CreateDirectory(testDirectory);
+            }
+
+            SQLiteConnection.CreateFile(testDirectory + @"\TestDB");
+        }
+
+        [OneTimeTearDown]
+        public void RemoveDatabase()
+        {
+            if (Directory.Exists(testDirectory))
+            {
+                Directory.Delete(testDirectory, true);
+            }
+        }
+    }
+}
